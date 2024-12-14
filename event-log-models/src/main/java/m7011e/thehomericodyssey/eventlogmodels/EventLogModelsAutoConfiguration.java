44fe0b9b7a2392b1e8afb931c3b77dd6406e281c
johan <itgsod.johan.holmgren@gmail.com>
2024-12-14 13:11:51 +0100

    Fix gradle

--- event-log-models/src/main/java/m7011e/thehomericodyssey/eventlogmodels/EventLogModelsAutoConfiguration.java
+++ event-log-models/src/main/java/m7011e/thehomericodyssey/eventlogmodels/EventLogModelsAutoConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan
+@ComponentScan(basePackages = "m7011e.thehomericodyssey.eventlogmodels")
 @EnableFeignClients
 public class EventLogModelsAutoConfiguration {
 }
