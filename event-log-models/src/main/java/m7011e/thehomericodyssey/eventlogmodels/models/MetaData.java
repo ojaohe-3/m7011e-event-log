ea878ee6a92a2e33ce26f2c0620ded47821c1e7a
Johan Rodahl Holmgren <itgsod.johan.holmgren@gmail.com>
2024-12-30 00:50:03 +0100

    Added some lombok details

--- event-log-models/src/main/java/m7011e/thehomericodyssey/eventlogmodels/models/MetaData.java
+++ event-log-models/src/main/java/m7011e/thehomericodyssey/eventlogmodels/models/MetaData.java
@@ -1,11 +1,17 @@
 package m7011e.thehomericodyssey.eventlogmodels.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class MetaData {
     private UUID id;
 
