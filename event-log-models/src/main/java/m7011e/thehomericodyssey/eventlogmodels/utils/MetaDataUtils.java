0fa5a10c3d98dcb0f20da8319e41be55e444d88e
Johan Rodahl Holmgren <itgsod.johan.holmgren@gmail.com>
2025-01-08 22:16:13 +0100

    Fix bugs in eventlog

--- event-log-models/src/main/java/m7011e/thehomericodyssey/eventlogmodels/utils/MetaDataUtils.java
+++ event-log-models/src/main/java/m7011e/thehomericodyssey/eventlogmodels/utils/MetaDataUtils.java
@@ -1,27 +1,86 @@
 package m7011e.thehomericodyssey.eventlogmodels.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import io.swagger.v3.core.util.ObjectMapperFactory;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.experimental.UtilityClass;
 import m7011e.thehomericodyssey.eventlogmodels.models.MetaData;
+import org.apache.commons.lang3.StringUtils;
 
 @UtilityClass
 public class MetaDataUtils {
 
-  public Set<MetaData> createMetaDataSetFromValue(String value) {
-    Set<MetaData> metaDataSet = new HashSet<>();
+  private static final int MAX_CHUNK_SIZE = 2048;
 
-    for (int i = 0; i < value.length() / 2048; i++) {
-      MetaData metaData =
-          MetaData.builder().value(value.substring(i * 2048, (i + 1) * 2048)).build();
-      metaDataSet.add(metaData);
+  /**
+   * Splits a string into chunks of maximum 2048 characters, preserving word boundaries.
+   * If a single word is longer than 2048 characters, it will be split at the 2048 character limit.
+   *
+   * @param input The input string to be split into chunks
+   * @return List of strings, each no longer than 2048 characters
+   * @throws IllegalArgumentException if input is null
+   */
+  public List<MetaData> createMetaDataSetFromValue(String input) {
+    if (input == null) {
+      throw new IllegalArgumentException("Input string cannot be null");
     }
-    return metaDataSet;
+
+    List<MetaData> chunks = new ArrayList<>();
+
+    if (input.length() <= MAX_CHUNK_SIZE) {
+      chunks.add(stringToMetaData(input));
+      return chunks;
+    }
+
+    int startIndex = 0;
+    while (startIndex < input.length()) {
+      int endIndex = Math.min(startIndex + MAX_CHUNK_SIZE, input.length());
+
+      if (endIndex < input.length()) {
+        int lastSpace = input.lastIndexOf(' ', endIndex);
+
+        if (lastSpace > startIndex && (endIndex - lastSpace) < MAX_CHUNK_SIZE / 4) {
+          endIndex = lastSpace;
+        }
+      }
+
+      String chunk = StringUtils.substring(input, startIndex, endIndex).trim();
+
+      if (StringUtils.isNotEmpty(chunk)) {
+        chunks.add(stringToMetaData(chunk));
+      }
+
+      startIndex = endIndex;
+
+      while (startIndex < input.length() && input.charAt(startIndex) == ' ') {
+        startIndex++;
+      }
+    }
+
+    return chunks;
+  }
+
+  private static MetaData stringToMetaData(String input) {
+    return MetaData.builder().value(input).build();
+  }
+
+  /**
+   * Convenience method to check if a string needs to be chunked
+   *
+   * @param input The input string to check
+   * @return true if the string exceeds the maximum chunk size
+   */
+  public boolean needsChunking(String input) {
+    return input != null && input.length() > MAX_CHUNK_SIZE;
   }
 
   public String javaObjectToString(Object object) throws JsonProcessingException {
-    return ObjectMapperFactory.buildStrictGenericObjectMapper().writeValueAsString(object);
+    ObjectMapper mapper = JsonMapper.builder()
+            .findAndAddModules()
+            .build();
+    return mapper.writeValueAsString(object);
   }
 }
